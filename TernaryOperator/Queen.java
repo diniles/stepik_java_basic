11593238df8ed5c5a5c36dc1ca923def9f0bda87
Dzmitry Liashko <dzliashko@gmail.com>
2023-02-09 13:01:00 +0300

    Add: class Queen

--- TernaryOperator/Queen.java
+++ TernaryOperator/Queen.java
@@ -39,8 +39,6 @@ public class Queen {
         int y1 = sc.nextInt();
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
-        int s = Math.abs(x1 - x2);
-        int t = Math.abs(y1 - y2);
         System.out.print(Math.abs(x1 - x2) == Math.abs(y1 - y2) || x1 == x2 || y1 == y2 ? "YES" : "NO");
     }
 }
